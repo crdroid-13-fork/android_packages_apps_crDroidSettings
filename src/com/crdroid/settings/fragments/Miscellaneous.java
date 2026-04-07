57d288cfc8d4d95e8794121536d85b2aa8f9d46b
muralivijay <muralidharan9845@gmail.com>
2026-04-07 17:07:46 +0530

    Revert "CrDroidSettings: Add restart util on preference change for safetynet attestation blocking"
    This reverts commit a7743527383f161f789e6aacbd57178159ab4cf8.

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -63,7 +63,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     private static final String POCKET_JUDGE = "pocket_judge";
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
-    private static final String SYS_PROP_OPTIONS_PI = "persist.sys.pixelprops.pi";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
     private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
     private static final String SYS_GAMEPROP_ENABLED = "persist.sys.gameprops.enabled";
@@ -71,8 +70,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     private static final String KEY_PIF_JSON_FILE_PREFERENCE = "pif_json_file_preference";
 
     private Preference mPocketJudge;
-    private Preference mPropOptionsPi;
-
     private Preference mPifJsonFilePreference;
 
     private Preference mGamePropsJsonFilePreference;
@@ -90,12 +87,8 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         final Resources res = getResources();
 
         mPifJsonFilePreference = findPreference(KEY_PIF_JSON_FILE_PREFERENCE);
-        mGamePropsJsonFilePreference = findPreference(KEY_GAME_PROPS_JSON_FILE_PREFERENCE);
+        mGamePropsJsonFilePreference = findPreference(KEY_GAME_PROPS_JSON_FILE_PREFERENCE);    
         mGamePropsSpoof.setOnPreferenceChangeListener(this);
-
-        mPropOptionsPi = (Preference) findPreference(SYS_PROP_OPTIONS_PI);
-        mPropOptionsPi.setOnPreferenceChangeListener(this);
-
         mPocketJudge = (Preference) prefScreen.findPreference(POCKET_JUDGE);
         boolean mPocketJudgeSupported = res.getBoolean(
                 com.android.internal.R.bool.config_pocketModeSupported);
@@ -248,7 +241,7 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mGamePropsSpoof || preference == mPropOptionsPi) {
+        if (preference == mGamePropsSpoof) {
                     SystemRestartUtils.showSystemRestartDialog(getContext());
             return true;
         }
